702d1c4164a7ce157bf29161ee44b947dbd27d3f
Brandon Michaux <michaux@Brandons-MacBook.local>
2018-03-06 17:17:03 -0500

    final upload

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friends = [
+  {
+    name: "Jamie",
+    photo: "http://www.tvguide.com/news/8-people-you-need-to-know-to-jump-right-into-power-at-season-3/",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Angela",
+    photo: "http://www.thecoli.com/threads/power-fans-tasha-vs-angela-who-would-you-pick.326090/",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Tasha",
+    photo: "http://www.blackbeautyandhair.com/naturi-naughtons-most-powerful-hairstyles/",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  }
+  
+ 
+ 
+
+];
+
+// export the array
+module.exports = friends;
--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use('/static', express.static(path.join(__dirname, 'app/public')))
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./app/routing/api-routes.js")(app);
-require("./app/routing/html-routes.js")(app);
+require("./app/routing/otherRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 
